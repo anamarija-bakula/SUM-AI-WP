b5869c01086a71563e54c593cd3bb8c3d497c864
anamarija-bakula <anamarijabakula01@gmail.com>
2024-01-24 12:55:13 +0100

    SUM AI

---
+++ app/public/wp-content/plugins/coming-soon/languages/seedprod-lite.php
@@ -0,0 +1,115 @@
+<?php
+/* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
+$generated_i18n_strings = array(
+	// Reference: src/views/TemplateChooser-Lite.vue:1329
+	__( '&larr; Go Back', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1330
+	__( '&larr; Go to Dashboard', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1331
+	__( 'Choose a New Page Template', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1332
+	__( 'All Templates', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1333
+	__( 'Favorite Templates', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1334
+	__( 'Saved Templates', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1335
+	__( 'No Favorited Templates Found', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1336
+	__( 'No Saved Templates Found', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1337
+	__( 'Filter:', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1338
+	__( 'All', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1339
+	__( 'No Templates Found', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1341
+	__( 'Choose This Template', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1342
+	__( 'Enter your new page details', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1343
+	__( 'You can always change it later in Page Settings.', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1347
+	__( 'Page Name:', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1348
+	__( 'Page URL:', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1349
+	__( 'Save and Start Editing the Page', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1353
+	__( 'First Page', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1354
+	__( 'Prev', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1355
+	__( 'Next', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1356
+	__( 'Last Page', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1357
+	__( 'Search templates...', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1358
+	__( 'My Landing Page name goes here', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1362
+	__( 'You can favorite any template by clicking the heart icon under the page template.', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1366
+	__( 'You can save pages as templates in the builder. Any saved pages will be shown here.', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1370
+	__( 'Send Us Your Email and Get 10 Free Templates', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1371
+	__( 'Get <strong>FREE</strong> Templates', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1372
+	__( 'PRO', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1373
+	__( 'SUBSCRIBE ABOVE TO UNLOCK', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1467
+	__( 'You now have access to 10 FREE templates.', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1530
+	__( 'Are you sure you want to delete?', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1539
+	__( 'Yes, delete it!', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1543
+	__( 'Cancel', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1593
+	__( 'Deleted!', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1756
+	__( 'Blank Template', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:1926
+	__( 'This page url already exisits. Please choose a unique page url.', 'coming-soon' ),
+
+	// Reference: src/views/TemplateChooser-Lite.vue:2048
+	__( 'Could not be saved. Please contact Support if you continue to experience this issue.', 'coming-soon' )
+);
+/* THIS IS THE END OF THE GENERATED FILE */
